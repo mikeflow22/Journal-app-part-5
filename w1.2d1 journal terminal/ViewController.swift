1b927d4acea4bc11e86ce47fdc286fbbe114aacc
Michael R.F <mrflowers22@uwalumni.com>
2018-01-22 18:05:30 -0700

    Initial Commit

---
+++ w1.2d1 journal terminal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w1.2d1 journal terminal
+//
+//  Created by Michael Flowers on 1/22/18.
+//  Copyright © 2018 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
