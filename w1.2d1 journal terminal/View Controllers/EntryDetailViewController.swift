78dffcbca6506db1e22d10fcd797c49afedb0330
Michael R.F <mrflowers22@uwalumni.com>
2018-01-22 19:36:00 -0700

    added the save button's function

--- w1.2d1 journal terminal/View Controllers/EntryDetailViewController.swift
+++ w1.2d1 journal terminal/View Controllers/EntryDetailViewController.swift
@@ -13,6 +13,7 @@ class EntryDetailViewController: UIViewController , UITextFieldDelegate {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var bodyTextView: UITextView!
     
+    var entry: Entry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,22 @@ class EntryDetailViewController: UIViewController , UITextFieldDelegate {
        return true
     }
     
+    //the logic behind this button is that we want to check if there is anything in it. If there is anything in it we want to know if its a new entry or old entry. if it's new then we add entry if it's not then we update entry
     @IBAction func savedButtonTapped(_ sender: UIBarButtonItem) {
+        //i remember that we checked for both title and bodyText string separately
+        guard let title = titleTextField.text else {return}
+        guard let bodyText = bodyTextView.text else {return}
+        
+        //now we check for whether we should add or update
+        
+        guard let newEntry =  entry else {
+            //if it is new entry we have to add it
+            EntryController.shared.addEntryWith(title: title, bodyText: bodyText)
+            navigationController?.popViewController(animated: true)
+            return}
+        //if not new entry then we update it
+        EntryController.shared.update(title: title, bodyText: bodyText, entry: newEntry)
+        navigationController?.popViewController(animated: true)
     }
     /*
     // MARK: - Navigation
